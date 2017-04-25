29cbcb8f191ff5e36ade50f1a9355464e4da95d1
PS-DEV <mahmod115.ma@gmail.com>
2017-04-25 15:38:55 +0300

    Enable VR

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -10,6 +10,7 @@ PRODUCT_PACKAGES += \
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
   
